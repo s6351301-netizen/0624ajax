544bb8715d073d01c2173972fa55179b9d3091f7
may <s6351301@gmail.com>
2026-06-25 11:16:17 +0800

    再練習三種網頁傳遞方式

---
+++ s20260624/s20260624_08_calc_data.php
@@ -0,0 +1,51 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$input = $_GET;
+
+// http://localhost/s20260624_php/s20260624_06_calc_data.php?num1=100&num2=50&opt='+';
+// dd($input);
+
+// $data = [
+//     'num1' => 100,
+//     'num2' => 50,
+//     'opt' => '+'
+// ];
+
+$data = $input;
+
+$num1 = $data['num1'];
+$num2 = $data['num2'];
+$opt = $data['opt'];
+$result = 0;
+switch ($opt) {
+    case '+':
+        $result = $num1 + $num2;
+        break;
+    case '-':
+        $result = $num1 - $num2;
+        break;
+    case '*':
+        $result = $num1 * $num2;
+        break;
+    case '/':
+        $result = $num1 / $num2;
+        break;
+
+    default:
+        # code...
+        break;
+}
+
+$data['result'] = $result;
+
+dd($data);
+
+// to json
+
+echo json_encode($data);
